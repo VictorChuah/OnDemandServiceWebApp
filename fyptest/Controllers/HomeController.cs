5c060013b4dcd8ea4d6ccb04c1ef3599e0a2a749
OnDemSer <OnDemand.Service.2021@gmail.com>
2021-11-10 01:55:32 +0800

    Signal R resources

--- fyptest/Controllers/HomeController.cs
+++ fyptest/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
+using fyptest.Models;
+using fyptest.SignalR.Hubs;
+using Microsoft.AspNet.SignalR;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +12,7 @@ namespace fyptest.Controllers
 {
   public class HomeController : Controller
   {
+    ServerDBEntities db = new ServerDBEntities();
     // GET: Home
     public ActionResult Index()
     {
@@ -16,6 +21,5 @@ namespace fyptest.Controllers
       TempData["Info"] = "test";
       return View();
     }
-
   }
 }
